0f08553184e711f4c156a034c9833d34ca6826f9
Tim Chenskiy <timadraft@gmail.com>
2020-05-08 18:27:21 +0300

    serializers added

---
+++ backend/core/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework.serializers import ModelSerializer
+from core.models import *
+
+
+class VesselTypeSerializer(ModelSerializer):
+    class Meta:
+        model = VesselType
+        fields = ('__all__')
+
+
+class VesselBuildSerializer(ModelSerializer):
+    class Meta:
+        model = VesselBuild
+        fields = ('__all__')
+
+
+class VesselFlagSerializer(ModelSerializer):
+    class Meta:
+        model = VesselFlag
+        fields = ('__all__')
+
+
+class VesselSerializer(ModelSerializer):
+    class Meta:
+        model = Vessel
+        fields = ('__all__')
+
+
+class PortCountrySerializer(ModelSerializer):
+    class Meta:
+        model = PortCountry
+        fields = ('__all__')
+
+
+class PortSerializer(ModelSerializer):
+    class Meta:
+        model = Port
+        fields = ('__all__')
+
+
+class RouteSerializer(ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ('__all__')
+
+
+class DateSerializer(ModelSerializer):
+    class Meta:
+        model = Date
+        fields = ('__all__')
+        
+
+class VoyageSerializer(ModelSerializer):
+    class Meta:
+        model = Voyage
+        fields = ('__all__')
