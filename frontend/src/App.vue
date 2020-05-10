e001e538428cf6c101c21b15395558f9eda9488b
Tim Chenskiy <timadraft@gmail.com>
2020-05-10 14:15:14 +0300

    navbar added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="indigo darken-1"
+      dark
+    >
+      <v-toolbar-title>Рейсы</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        Вход
+        <v-icon right>mdi-login</v-icon>
+      </v-btn>
+
+      <v-btn text>
+        Регистрация
+        <v-icon right>mdi-account-plus</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
