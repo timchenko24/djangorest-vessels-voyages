7c00b406bc8b09bb494579228c5bd4263d08465c
Tim Chenskiy <timadraft@gmail.com>
2020-05-15 12:30:21 +0300

    footer added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,6 +24,41 @@
     <v-content>
       <router-view/>
     </v-content>
+
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="white--text text-center"
+        color="#587796"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus
+          commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at
+          orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <i>Chenskiy Timofey</i>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
@@ -33,7 +68,12 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    icons: [
+      'mdi-facebook',
+      'mdi-vk',
+      'mdi-linkedin',
+      'mdi-instagram',
+    ],
   }),
 };
 </script>
