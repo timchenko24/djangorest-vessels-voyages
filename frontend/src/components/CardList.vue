b6599c154afd6db2d83b69b2210cf250649f2830
Tim Chenskiy <timadraft@gmail.com>
2020-05-15 11:52:11 +0300

    CardList component added

---
+++ frontend/src/components/CardList.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row class="my-10">
+
+    <v-col
+      v-for="(item, index) in items"
+      :key="index"
+      cols="12"
+      :sm="division"
+    >
+      <Card :title="item.title" :index="(index+1)">
+
+        <template v-slot:img>
+
+          <slot name="img" :path="item"></slot>
+
+        </template>
+
+        <template v-slot:content>
+
+          <slot name="content" :path="item"></slot>
+
+        </template>
+
+      </Card>
+
+    </v-col>
+
+  </v-row>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'CardList',
+  components: { Card },
+  props: {
+    items: {
+      type: Object,
+      required: true,
+    },
+    division: {
+      type: Number,
+    },
+  },
+  data: () => ({
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
