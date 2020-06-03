e0bec3358e7f09445a12131e6b29dab0510c75c2
Tim Chenskiy <timadraft@gmail.com>
2020-06-03 16:33:50 +0300

    links added

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -38,11 +38,16 @@
 
       </template>
 
+
       <template v-slot:title="titleScope">
 
-        <v-card-title>
-          {{ titleScope.item.title }}
-        </v-card-title>
+        <router-link :to="titleScope.item.path">
+
+          <v-card-title>
+            {{ titleScope.item.title }}
+          </v-card-title>
+
+        </router-link>
 
       </template>
 
@@ -75,22 +80,26 @@ export default {
           icon: 'mdi-ferry',
           text: 'Основная информация о судах: номер MMSI, IMO, название, национальность, '
               + 'год постройки, позывной, длина, ширина, грузоподъемность, дедвейт',
+          path: '/vessels',
         },
         {
           title: 'Порты',
           icon: 'mdi-anchor',
           text: 'Основная информация о портах: страна, название, тип, широта, долгота',
+          path: '/ports',
         },
         {
           title: 'Маршруты',
           icon: 'mdi-compass',
           text: 'Список маршрутов судов в формате Порт отправления - Порт назначения',
+          path: '/routes',
         },
         {
           title: 'Рейсы',
           icon: 'mdi-buddhism',
           text: 'Основная информация о каждом рейсе судна: дата отбытия, дата прибытия в порт,'
               + 'время в порту, затраты на топливо, завтраты на экипаж, портовые расходы и т.д.',
+          path: '/voyages',
         },
       ],
     };
