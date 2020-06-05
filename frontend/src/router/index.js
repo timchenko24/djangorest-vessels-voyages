fa3c40987ebdfa03c708b40dad027c4c22af7d4c
Tim Chenskiy <timadraft@gmail.com>
2020-06-05 14:03:20 +0300

    /routes path added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     name: 'PortIndex',
     component: () => import('../views/ports/Index.vue'),
   },
+  {
+    path: '/routes',
+    name: 'RouteIndex',
+    component: () => import('../views/routes/Index.vue'),
+  },
 ];
 
 const router = new VueRouter({
