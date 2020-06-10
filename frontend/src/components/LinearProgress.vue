ad74610cf2ec0680a73ebd08a5c5e451429e6aa4
Tim Chenskiy <timadraft@gmail.com>
2020-06-10 12:57:25 +0300

    LinearProgress added

---
+++ frontend/src/components/LinearProgress.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div v-if="loading" class="text-center">
+
+    <v-progress-linear
+      indeterminate
+      :height='height'
+      absolute
+      top
+      :color='color'>
+    </v-progress-linear>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'LinearProgress',
+
+  props: {
+
+    height: {
+      type: [Number, String],
+      required: true,
+    },
+
+    color: {
+      type: String,
+      required: true,
+    },
+
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
