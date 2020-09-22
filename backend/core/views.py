a6846ff719593d5b188dd79c0d58d0c92483987c
Tim Chenskiy <timadraft@gmail.com>
2020-09-22 12:28:06 +0300

    init

--- backend/core/views.py
+++ backend/core/views.py
@@ -145,6 +145,28 @@ class VoyageViewSet(ModelViewSet):
     serializer_class = VoyageSerializer
     queryset = Voyage.objects.all()
 
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = {'route': ['in'],
+                        'mmsi': ['in'],
+                        'departure_date': ['in'],
+                        'arrival_date': ['in'],
+                        'time_in_port': ['gte', 'lte'],
+                        'fuel_costs': ['gte', 'lte'],
+                        'crew_costs': ['gte', 'lte'],
+                        'port_charges': ['gte', 'lte'],
+                        'insurance_costs': ['gte', 'lte'],
+                        'total_costs': ['gte', 'lte'],
+                        'cargo_income': ['gte', 'lte'],
+                        'net_total_freight': ['gte', 'lte'],
+                        'voyage_profit': ['gte', 'lte'],
+                        }
+    ordering_fields = ['route', 'mmsi', 'departure_date', 'arrival_date', 'time_in_port', 'fuel_costs', 'crew_costs',
+                       'port_charges', 'insurance_costs', 'total_costs', 'cargo_income', 'net_total_freight',
+                       'voyage_profit']
+    search_fields = ['route', 'mmsi', 'departure_date', 'arrival_date', 'time_in_port', 'fuel_costs', 'crew_costs',
+                     'port_charges', 'insurance_costs', 'total_costs', 'cargo_income', 'net_total_freight',
+                     'voyage_profit']
+
     def get_permissions(self):
 
         if self.action == 'list':
--- frontend/src/views/voyages/Index.vue
+++ frontend/src/views/voyages/Index.vue
@@ -1,30 +1,77 @@
 <template>
+
   <v-container fluid>
 
-    <LinearProgress :height="6" color="#FBA846" :loading="loading"></LinearProgress>
+    <v-layout row wrap>
 
-    <h1 class="display-3 text-center pt-10">Рейсы</h1>
-    <hr/>
+      <LinearProgress :height="6" color="#FBA846" :loading="loading"></LinearProgress>
 
-    <CardList :items="titledVoyages" :division="6">
+      <v-flex md3 class="pa-5">
 
-      <template v-slot:title="titleScope">
+        <v-layout row wrap>
 
-        <v-card-title class="font-weight-black indigo lighten-5">
-          {{ titleScope.item.route.value }}
-        </v-card-title>
+          <Sidebar>
 
-      </template>
+            <template v-slot:content>
 
-      <template v-slot:content="contentScope">
+              <v-flex md12>
+                <MultiSelect @onAbort="abortSelectByType" @onChange="updateByType" label='Тип судна'
+                             :filterBy="Object.keys(filters.route.dict)">
+                </MultiSelect>
+              </v-flex>
 
-        <Tile :items="contentScope.item"></Tile>
+              <v-flex md12>
+                <MultiSelect @onAbort="abortSelectByFlag" @onChange="updateByFlag" label='Флаг'
+                             :filterBy="Object.keys(filters.vessel.dict)">
+                </MultiSelect>
+              </v-flex>
 
-      </template>
 
-    </CardList>
+            </template>
+
+          </Sidebar>
+
+        </v-layout>
+
+      </v-flex>
+
+      <v-flex md9 class="pa-6">
+
+        <v-layout row wrap>
+
+          <v-flex md12 class="pa-4">
+
+            <h1 class="display-3 text-center pt-10">Рейсы</h1>
+            <hr/>
+
+            <CardList :items="titledVoyages" :division="6">
+
+                <template v-slot:title="titleScope">
+
+                  <v-card-title class="font-weight-black indigo lighten-5">
+                    {{ titleScope.item.route.value }}
+                  </v-card-title>
+
+                </template>
+
+                <template v-slot:content="contentScope">
+
+                  <Tile :items="contentScope.item"></Tile>
+
+                </template>
+
+              </CardList>
+
+          </v-flex>
+
+        </v-layout>
+
+      </v-flex>
+
+    </v-layout>
 
   </v-container>
+
 </template>
 
 <script>
@@ -32,13 +79,21 @@ import axios from 'axios';
 import CardList from '../../components/CardList.vue';
 import Tile from '../../components/Tile.vue';
 import LinearProgress from '../../components/LinearProgress.vue';
-
 import titler from '../../components/mixins/titler';
+import MultiSelect from '../../components/MultiSelect.vue';
+// import RangeSelect from '../../components/RangeSelect.vue';
+import Sidebar from '../../components/Sidebar.vue';
+// import SortBy from '../../components/SortBy.vue';
+// import Search from '../../components/Search.vue';
+import pagination from '../../components/mixins/pagination';
+import paramQueryGenerator from '../../components/mixins/paramQueryGenerator';
 
 export default {
   name: 'Index',
-  components: { CardList, Tile, LinearProgress },
-  mixins: [titler],
+  components: {
+    CardList, Tile, LinearProgress, MultiSelect, Sidebar, // SortBy, Search, RangeSelect
+  },
+  mixins: [titler, pagination, paramQueryGenerator],
 
   data() {
     return {
@@ -47,22 +102,158 @@ export default {
       titles: ['Маршрут', 'Судно', 'Дата отбытия', 'Дата прибытия', 'Время в порту (час)', 'Затраты на топливо ($)',
         'Затраты на экипаж ($)', 'Портовые расходы ($)', 'Расходы на страхование ($)', 'Сумма расходов ($)',
         'Доход от перевозки груза ($)', 'Чистый суммарный фрахт ($)', 'Прибыль за рейс ($)'],
+      routes: {},
+      dates: {},
+      vessels: {},
       loading: true,
+
+      filters: {
+        route: {
+          dataSet: [],
+          dict: {},
+          range: false,
+          string: 'route__in=',
+          title: 'Маршрут',
+          enabled: false,
+        },
+        vessel: {
+          dataSet: [],
+          dict: {},
+          range: false,
+          string: 'mmsi__in=',
+          title: 'Судно',
+          enabled: false,
+        },
+        departure_date: {
+          dataSet: [],
+          dict: {},
+          range: false,
+          string: 'departure_date__in=',
+          title: 'Дата отправления',
+          enabled: false,
+        },
+        arrival_date: {
+          dataSet: [],
+          dict: {},
+          range: false,
+          string: 'arrival_date__in=',
+          title: 'Дата отправления',
+          enabled: false,
+        },
+        port_time: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['time_in_port__gte=', 'time_in_port__lte='],
+          title: 'Время в порту',
+          enabled: [false, false],
+        },
+        fuel_costs: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['fuel_costs__gte=', 'fuel_costs__lte='],
+          title: 'Затраты на топливо',
+          enabled: [false, false],
+        },
+        crew_costs: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['crew_costs__gte=', 'crew_costs__lte='],
+          title: 'Затраты на экипаж',
+          enabled: [false, false],
+        },
+        port_charges: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['port_charges__gte=', 'port_charges__lte='],
+          title: 'Портовые расходы',
+          enabled: [false, false],
+        },
+        insurance_costs: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['insurance_costs__gte=', 'insurance_costs__lte='],
+          title: 'Затраты на страховку',
+          enabled: [false, false],
+        },
+        total_costs: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['total_costs__gte=', 'total_costs__lte='],
+          title: 'Сумма затрат',
+          enabled: [false, false],
+        },
+        cargo_income: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['cargo_income__gte=', 'cargo_income__lte='],
+          title: 'Доход от перевозки груза',
+          enabled: [false, false],
+        },
+        net_total_freight: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['net_total_freight__gte=', 'net_total_freight__lte='],
+          title: 'Чистый суммарный фрахт',
+          enabled: [false, false],
+        },
+        voyage_profit: {
+          dataSet: [],
+          dict: {},
+          range: true,
+          limits: [],
+          string: ['voyage_profit__gte=', 'voyage_profit__lte='],
+          title: 'Прибыль за рейс',
+          enabled: [false, false],
+        },
+      },
     };
   },
 
   methods: {
     async getData() {
-      const path = 'http://127.0.0.1:8000/api/voyage/';
-      const wholeResponse = await axios.get(path);
-      this.loading = false;
+      const path = 'http://localhost:8000/api';
+      let wholeResponse = await axios.get(`${path}/voyage/`);
       this.voyages = wholeResponse.data;
+
+      wholeResponse = await axios.get(`${path}/route/`);
+      this.routes = wholeResponse.data;
+      this.filters.route.dict = Object.assign({}, ...this.routes
+        .map((x) => ({ [x.departure_port]: x.id })));
+
+      wholeResponse = await axios.get(`${path}/date/`);
+      this.date = wholeResponse.data;
+      this.filters.date.dict = Object.assign({}, ...this.date
+        .map((x) => ({ [x.minute]: x.id })));
+
+      wholeResponse = await axios.get(`${path}/vessel/`);
+      this.vessels = wholeResponse.data;
+      this.filters.vessel.dict = Object.assign({}, ...this.vessels
+        .map((x) => ({ [x.name]: x.mmsi })));
+
+      this.titledVoyages = this.setTitles(this.voyages, this.titles, ['id']);
+
+      this.loading = false;
     },
   },
 
   async mounted() {
     await this.getData();
-    this.titledVoyages = this.setTitles(this.voyages, this.titles, ['id']);
   },
 };
 </script>
