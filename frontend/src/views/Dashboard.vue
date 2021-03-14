deaf1e84b9d09ff10ebb5ff5b7eacc48fd7045a1
Tim Chenskiy <timadraft@gmail.com>
2021-03-14 18:54:23 +0300

    main dashboard view

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,11 +1,19 @@
 <template>
-    $END$
+  <v-container>
+    <v-row class="pt-10">
+
+      <v-col cols="12">
+        <h3>Dashboard</h3>
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: "Dashboard"
-    }
+export default {
+  name: 'Dashboard',
+};
 </script>
 
 <style scoped>
