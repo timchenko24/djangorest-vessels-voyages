34882433114b49585a4a0d16550ea066258f5b5a
Tim Chenskiy <timadraft@gmail.com>
2021-04-02 18:16:38 +0300

    available queries view

--- backend/ml/urls.py
+++ backend/ml/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import AvailableQueries
 
 
 urlpatterns = [
-    path('clustering/', include('ml.clustering.urls'))
+    path('clustering/', include('ml.clustering.urls')),
+    path('clustering/', AvailableQueries.as_view())
 ]
---
+++ backend/ml/views.py
@@ -0,0 +1,14 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .queries import queries
+
+
+class AvailableQueries(APIView):
+
+    def get(self, request):
+        titles = ['Дедвейт - Затраты на топливо', 'Длина судна - Прибыль за рейс']
+
+        res = [{query: title} for query, title in zip(queries.keys(), titles)]
+
+        return Response({'result': res}, status=status.HTTP_200_OK)
